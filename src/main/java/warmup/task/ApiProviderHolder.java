26cd2fc29ff07519292259b55d113b9062a4e087
vladimir <smagellan@gmail.com>
2016-05-31 17:56:50 +0300

    removing unnecessary file

--- src/main/java/warmup/task/ApiProviderHolder.java
+++
@@ -1,8 +0,0 @@
-package warmup.task;
-
-/**
- * Created by vladimir on 5/26/16.
- */
-public class ApiProviderHolder {
-    public static final ApiInterfaceImpl instance = new ApiInterfaceImpl();
-}
